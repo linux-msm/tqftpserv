9ae1eb3a9abc42d05411bd938cd2e72132da264e
Luca Weiss <luca.weiss@fairphone.com>
2024-01-19 14:25:34 +0100

    tqftpserv: don't print "End of Transfer" as an error
    On newer modems, the firmware seems to use a stat-like operation
    relatively often which end the transfer with error 9 "End of Transfer".
    
    In practise we're getting a RRQ with the tsize option set, we'll query
    the file size from the filesystem, respond with the updated tsize in
    OACK, then the modem will send us OP_ERROR with code 9 "End of
    Transfer".
    
    Since that's expected in this operation, let's not make it look like an
    error to not confuse users/developers trying to debug the modem.

--- tqftpserv.c
+++ tqftpserv.c
@@ -486,7 +486,12 @@ static int handle_reader(struct tftp_client *client)
 	opcode = buf[0] << 8 | buf[1];
 	if (opcode == OP_ERROR) {
 		buf[len] = '\0';
-		printf("[TQFTP] Remote returned an error: %d - %s\n", buf[2] << 8 | buf[3], buf + 4);
+		int err = buf[2] << 8 | buf[3];
+		/* "End of Transfer" is not an error, used with stat(2)-like calls */
+		if (err == 9)
+			printf("[TQFTP] Remote returned END OF TRANSFER: %d - %s\n", err, buf + 4);
+		else
+			printf("[TQFTP] Remote returned an error: %d - %s\n", err, buf + 4);
 		return -1;
 	} else if (opcode != OP_ACK) {
 		printf("[TQFTP] Expected ACK, got %d\n", opcode);
