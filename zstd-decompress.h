f9da0667fbcd99c4e0e9e5179f190e8dd9703702
Emil Velikov <emil.l.velikov@gmail.com>
2024-09-19 14:13:00 +0100

    Fix Android build without zstd
    Bit of a blind fix, since I don't have an Android dev setup nearby.
    
    Signed-off-by: Emil Velikov <emil.l.velikov@gmail.com>

--- zstd-decompress.h
+++ zstd-decompress.h
@@ -8,6 +8,14 @@
 
 #include <stdbool.h>
 
+#ifdef HAVE_ZSTD
 int zstd_decompress_file(const char *filename);
+#else
+static int zstd_decompress_file(const char *filename)
+{
+	fprintf(stderr, "Built without ZSTD support\n");
+	return -1;
+}
+#endif
 
 #endif
