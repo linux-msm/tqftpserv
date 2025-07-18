33a3b4891e19033458d19f47069e7db23751fc0f
Bhaskar Valaboju <quic_bhaskarv@quicinc.com>
2025-07-18 18:34:46 +0530

    tqftpserv: Cache wsize buffers in client instance
    The client sends data packets of blksize size and waits for an ACK
    after sending wsize packets. If packets are not received in sequence,
    the server should send NACK, prompting the client to resend the packets
    starting from beginning of the window.
    
    For example, with wsize = 10 and blksize = 5000, the client sends
    blocks [201–210] and waits. If all are received, the server replies
    with ACK; otherwise, it sends NACK, and the client restarts from block 201.
    
    Use rw_buf to cache received blocks and track the next expected block
    using blk_expected. Reset the expected block to the start of the window
    if out-of-sequence packets are detected.
    
    Once all buffers are received, commit the cached buffers to the file.
    
    Signed-off-by: Bhaskar Valaboju <quic_bhaskarv@quicinc.com>

--- tqftpserv.c
+++ tqftpserv.c
@@ -50,6 +50,8 @@ struct tftp_client {
 	uint8_t *blk_buf;
 	uint8_t *rw_buf;
 	size_t rw_buf_size;
+	size_t blk_offset;
+	uint16_t blk_expected;
 };
 
 static struct list_head readers = LIST_INIT(readers);
@@ -408,6 +410,7 @@ static void handle_wrq(const char *buf, size_t len, struct sockaddr_qrtr *sq)
 	client->timeoutms = timeoutms;
 	client->seek = seek;
 	client->rw_buf_size = blksize * wsize;
+	client->blk_expected = 1;
 
 	client->blk_buf = calloc(1, blksize + 4);
 	if (!client->blk_buf) {
@@ -545,15 +548,46 @@ static int handle_writer(struct tftp_client *client)
 	}
 
 	payload = len - 4;
+	buf += 4;
 
-	ret = write(client->fd, buf + 4, payload);
-	if (ret < 0) {
-		/* XXX: report error */
-		printf("[TQFTP] failed to write data\n");
+	/* Check if we recieved expected block */
+	if (block != client->blk_expected) {
+		uint16_t blk_expected = client->blk_expected;
+
+		printf("[TQFTP] Block number out of sequence: %d (expected %d)\n",
+			 block, blk_expected);
+		tftp_send_error(client->sock, 4, "Block number out of sequence");
+
+		/* Set blk_expected to beginning of current window */
+		if ((blk_expected % client->wsize) == 0)
+			blk_expected -= client->wsize + 1;
+		else
+			blk_expected -= (blk_expected % client->wsize) - 1;
+
+		client->blk_expected = blk_expected;
+		client->blk_offset = 0;
+
 		return -1;
 	}
 
-	tftp_send_ack(client->sock, block);
+	client->blk_expected++;
+
+	/* Copy the data to the destination buffer */
+	memcpy(client->rw_buf + client->blk_offset, buf, payload);
+	client->blk_offset += payload;
+
+	/* Write to file if all the wsize blocks are recieved */
+	if (block % client->wsize == 0) {
+		ret = write(client->fd, client->rw_buf, client->blk_offset);
+		if (ret < 0) {
+			/* XXX: report error */
+			printf("[TQFTP] failed to write data\n");
+			return -1;
+		}
+
+		client->blk_offset = 0;
+		tftp_send_ack(client->sock, block);
+	}
 
 	return payload == client->blksize ? 1 : 0;
 }
